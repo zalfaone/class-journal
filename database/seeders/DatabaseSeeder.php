a09fe993a96dff69e0cfb698700f706d79ce4fd8
Rizal Fathur Rahman <rizalfathurrahman0123@gmail.com>
2023-05-27 13:52:41 +0700

    call class seeder to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            SubjectsSeeder::class
+            SubjectsSeeder::class,
+            ClassSeeder::class
         ]);
     }
 }
