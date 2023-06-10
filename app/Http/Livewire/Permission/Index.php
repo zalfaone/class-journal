055716bf8f3916ee81f08834ad77005d8d2064c5
Rizal Fathur Rahman <rizalfathurrahman0123@gmail.com>
2023-06-10 10:34:03 +0700

    add user,role,and permission

---
+++ app/Http/Livewire/Permission/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Permission;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.permission.index');
+    }
+}
